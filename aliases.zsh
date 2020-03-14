0c607ebeb6311374498c4c31830a1fd186ce38d0
Champions007 <mareksrops53@gmail.com>
2020-03-15 00:27:37 +0200

    added more dotfiles

---
+++ aliases.zsh
@@ -0,0 +1,17 @@
+
+# My aliases
+alias zshrc="vim ~/.zshrc"
+alias YT="youtube-viewer"
+alias yt="youtube-dl --add-metadata -ic" #Download video 
+alias yta="youtube-dl --add-metadata -xic" #Download audio 
+alias lc="nmcli d wifi list" #Shows wifi hotspot list
+alias wirec="nmcli c up" #connects to wifi network name needed
+alias wired="nmcli c down" #disconnect from named wifi network
+alias vimrc="nvim ~/.config/nvim/init.vim" #Opens .vimrc file
+alias i3config="vim ~/.config/i3/config" #Opens i3config file.
+alias sqllogin="mysql -u root -p"
+alias notes="joplin"
+alias ll="l-alF"
+alias la="ls -A"
+alias l="ls -CF"
+
